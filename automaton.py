412f86949c88aeeda458c31f270b44d25456b7fc
DavidPatranjel <davidpatranjel24@gmail.com>
2022-03-23 22:14:33 +0200

    Proiect LFA alpha 1.0

--- automaton.py
+++ automaton.py
@@ -26,9 +26,55 @@ class Automaton():
         If the input is rejected, the method raises a
         RejectionException.
         """
+        sigma, states, transitions = [], [], []
+        f = open(input_str)
+        def readSigma(line):
+            line = f.readline().strip()
+            while not line == "End":
+                sigma.append(line)
+                line = f.readline().strip()
+
+        def readStates(line):
+            start = 0
+            line = f.readline().strip().replace(",", "")
+            while not line == "End":
+                aux = line.split()
+                if (len(aux) > 1):
+                    if (aux[1] == 'S'):
+                        start = start + 1
+                states.append(aux[0])
+                line = f.readline().strip().replace(",", "")
+            if start != 1:
+                print("Input is not valid")
+                exit(0)
+
+        def readTransitions(line):
+            line = f.readline().strip().replace(",", "")
+            while not line == "End":
+                aux = line.split()
+                transitions.append(tuple(aux))
+                line = f.readline().strip().replace(",", "")
+
+        for i in range(3):
+            line = f.readline()
+            while line.startswith("#"):
+                line = f.readline()
+            line = line.strip()
+            if line == "Sigma :":
+                readSigma(line)
+            elif line == "States :":
+                readStates(line)
+            elif line == "Transitions :":
+                readTransitions(line)
+
+        for transition in transitions:
+            if (transition[0] not in states) or (transition[1] not in sigma) or (transition[2] not in states):
+                print("Input is not valid")
+                exit(0)
+        print("Input is valid")
         pass
     
 
 if __name__ == "__main__":
-    a = Automaton('your_config_file')
-    print(a.validate())
+    a = Automaton('date.in')
+    a.read_input("date.in")
